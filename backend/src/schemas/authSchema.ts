b94ca92e9ac8d5c84af919527cc0fa322f4550a4
ev0 <ev0clu@gmail.com>
2025-01-30 08:58:36 +0100

    Auth schema is created for validation

---
+++ backend/src/schemas/authSchema.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+import libphonenumber from 'google-libphonenumber';
+
+const phoneUtil = libphonenumber.PhoneNumberUtil.getInstance();
+
+export const userRegistrationSchema = z.object({
+    username: z
+        .string()
+        .trim()
+        .min(4, 'Username is required to be min 4 characters')
+        .max(10, "Username shall be max 10 characters'"),
+    email: z.string().trim().email({ message: 'Email is invalid.' }),
+    password: z.string().trim().min(4, 'Password must have min 4 characters'),
+    phone: z
+        .string()
+        .trim()
+        .nonempty({ message: 'Phone number is required' })
+        .refine(
+            (number) => {
+                try {
+                    const phoneNumber = phoneUtil.parse(number);
+                    return phoneUtil.isValidNumber(phoneNumber);
+                } catch (error) {
+                    return false;
+                }
+            },
+            { message: 'Invalid phone number' }
+        )
+});
+
+export const userLoginSchema = z.object({
+    email: z.string().trim().email({ message: 'Email is invalid.' }),
+    password: z.string().trim().min(4, 'Password must have min 4 characters')
+});
