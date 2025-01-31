2ca70496f6a5f9afffe49e2cd6bf53105708887d
ev0 <ev0clu@gmail.com>
2025-01-31 16:57:05 +0100

    eslint config is updated

--- backend/eslint.config.js
+++
@@ -1,8 +0,0 @@
-export default [
-    {
-        rules: {
-            '@typescript-eslint/consistent-type-imports': 'error',
-            '@typescript-eslint/no-explicit-any': 'warn'
-        }
-    }
-];
--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,12 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    { ignores: [] },
+    {
+        rules: {
+            '@typescript-eslint/consistent-type-imports': 'error',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-unused-vars': 'error'
+        }
+    }
 ];
