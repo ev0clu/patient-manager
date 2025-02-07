a8cb441328aabd94568bd93e6a1e5d6aa272413b
ev0 <ev0clu@gmail.com>
2025-02-07 16:31:43 +0100

    Root layout is updated with appointments route

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -40,6 +40,10 @@ export default function RootLayout() {
             <Stack>
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
               <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+              <Stack.Screen
+                name="appointments/[id]"
+                options={{ headerShown: false }}
+              />
               <Stack.Screen name="index" options={{ headerShown: false }} />
               <Stack.Screen name="+not-found" />
             </Stack>
