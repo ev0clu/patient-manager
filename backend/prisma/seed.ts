940941649833a6988aba7c542358b631166dbc9a
ev0 <ev0clu@gmail.com>
2025-02-10 13:11:18 +0100

    Seed file is updated according to the new schema models

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,11 +1,13 @@
 import bcrypt from 'bcrypt';
 import prisma from './prisma';
 import { env } from '../src/utils/env';
+import { DOCTORS } from '../src/constants/doctors';
 
 async function main() {
     const adminHashedPassword = await bcrypt.hash(env.ADMIN_PASSWORD, Number(env.SALT_ROUNDS));
     const userHashedPassword = await bcrypt.hash(env.TEST_USER_PASSWORD, Number(env.SALT_ROUNDS));
 
+    // Create test user
     const admin = await prisma.user.create({
         data: {
             username: env.ADMIN_USERNAME,
@@ -16,6 +18,7 @@ async function main() {
         }
     });
 
+    // Create admin user
     const user = await prisma.user.create({
         data: {
             username: env.TEST_USERNAME,
@@ -25,7 +28,40 @@ async function main() {
         }
     });
 
-    console.log({ admin, user });
+    // Create doctors
+    await prisma.doctor.createMany({
+        data: DOCTORS.map((doctor) => ({
+            name: doctor.name,
+            image: doctor.image
+        }))
+    });
+
+    const doctors = await prisma.doctor.findMany();
+
+    // Create slots
+    const dates: Date[] = [];
+
+    for (let i = 0; i <= 3; i++) {
+        const date = new Date();
+        date.setDate(new Date().getDate() + 5 + i); // new date after 5 + i days from today
+        date.setHours(8 + i, 0, 0, 0); // set hour to 8am + i
+
+        dates.push(date);
+    }
+
+    for (const doctor of doctors) {
+        await prisma.slot.createMany({
+            data: dates.map((date) => ({
+                date: date,
+                booked: false,
+                doctorId: doctor.id
+            }))
+        });
+    }
+
+    const slots = await prisma.slot.findMany();
+
+    console.log({ admin, user, doctors, slots });
 }
 
 main()
