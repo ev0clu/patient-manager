a64cc38bc02a3a0fdc6f3fc0feae9cc19bb25df0
ev0 <ev0clu@gmail.com>
2025-02-11 16:59:28 +0100

    Contant is added to handle doctors information

---
+++ frontend/constants/doctors.ts
@@ -0,0 +1,22 @@
+export const DOCTORS = [
+  {
+    image: require("../assets/images/dr-cameron.png"),
+    name: "Jasmine Cameron",
+  },
+  {
+    image: require("../assets/images/dr-cruz.png"),
+    name: "Leila Cruz",
+  },
+  {
+    image: require("../assets/images/dr-green.png"),
+    name: "Evan Green",
+  },
+  {
+    image: require("../assets/images/dr-lee.png"),
+    name: "Alyana Lee",
+  },
+  {
+    image: require("../assets/images/dr-stone.png"),
+    name: "John Stone",
+  },
+];
