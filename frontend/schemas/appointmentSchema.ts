dafb3bf367af6649a69af6c41290aa5121c69783
ev0 <ev0clu@gmail.com>
2025-02-13 16:46:38 +0100

    Appointment schema is separated to create and update schema

--- frontend/schemas/appointmentSchema.ts
+++ frontend/schemas/appointmentSchema.ts
@@ -2,11 +2,18 @@ import { z } from "zod";
 
 const StatusValues = ["PENDING", "SCHEDULED", "CANCELLED"] as const;
 
-export const appointmentSchema = z.object({
+export const createAppointmentSchema = z.object({
   doctorId: z.string().trim().nonempty("Doctor Id is required"),
   description: z.optional(z.string().trim()),
   status: z.optional(z.enum(StatusValues)),
   slotId: z.string().trim().nonempty("Slot Id is required"),
 });
 
-export type appointmentType = z.infer<typeof appointmentSchema>;
+export type createAppointmentType = z.infer<typeof createAppointmentSchema>;
+
+export const updateAppointmentSchema = z.object({
+  description: z.optional(z.string().trim()),
+  status: z.optional(z.enum(StatusValues)),
+});
+
+export type updateAppointmentType = z.infer<typeof updateAppointmentSchema>;
