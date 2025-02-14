e6934a36b0686e3be605cd0b372f759fb257805d
ev0 <ev0clu@gmail.com>
2025-02-14 11:15:14 +0100

    Profile component is extended with user data and sign out component

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,21 +1,57 @@
 import ContainerView from "@/components/ContainerView";
-import { View, Text, ScrollView } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import { useAuthContext } from "@/context/AuthProvider";
+import { router } from "expo-router";
+import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Profile = () => {
+  const { setIsLogged, clearTokenStorage, userInfo } = useAuthContext();
+
+  if (!userInfo) {
+    <SafeAreaView className="h-full">
+      <ContainerView>
+        <Text className="text-3xl font-bold text-white mb-5">Profile</Text>
+        <View>
+          <Text className="text-white/50">Something went wrong!</Text>
+        </View>
+      </ContainerView>
+    </SafeAreaView>;
+  }
   return (
     <SafeAreaView className="h-full">
-      <ScrollView
-        contentContainerStyle={{
-          height: "100%",
-        }}
-      >
-        <ContainerView>
-          <View>
-            <Text className="text-white">profile</Text>
+      <ContainerView className="justify-start">
+        <Text className="text-3xl font-bold text-white mb-5">Profile</Text>
+        <View className="mb-5 w-full">
+          <View className="flex flex-row gap-2 mb-5">
+            <Text className="text-lg font-medium text-white">Username:</Text>
+            <Text className="text-lg font-semibold text-white/50">
+              {userInfo?.username}
+            </Text>
           </View>
-        </ContainerView>
-      </ScrollView>
+          <View className="flex flex-row gap-2 mb-5">
+            <Text className="text-lg font-medium text-white">Email:</Text>
+            <Text className="text-lg font-semibold text-white/50">
+              {userInfo?.email}
+            </Text>
+          </View>
+          <View className="flex flex-row gap-2">
+            <Text className="text-lg font-medium text-white">Role:</Text>
+            <Text className="text-lg font-semibold text-white/50">
+              {userInfo?.role}
+            </Text>
+          </View>
+        </View>
+        <View className="mx-auto mt-3">
+          <CustomButton
+            title="SIGN OUT"
+            onPress={() => {
+              setIsLogged(false);
+              clearTokenStorage(), router.push("/");
+            }}
+          />
+        </View>
+      </ContainerView>
     </SafeAreaView>
   );
 };
